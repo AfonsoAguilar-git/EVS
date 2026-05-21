9d87c0a2b38f06ad1654399b63edc2581b9b5a55
JooMchh <jrmos@iscte-iul.pt>
2026-05-21 18:41:12 +0100

    quick fix
    adjusted column sizing

--- Client/EVS/src/Pages/PollsPage.jsx
+++ Client/EVS/src/Pages/PollsPage.jsx
@@ -6,7 +6,7 @@ function PollsPage({setView, isLoggedIn, onlogout}){
         <section>
             <Navbar setView={setView} isLoggedIn={isLoggedIn} onlogout={onlogout}/>
                 <nav className="container-fluid p-4">
-                <ul className="row m-0 p-0 row-cols-2 row-cols-md-5 g-5">
+                <ul className="row m-0 p-0 row-cols-1 row-cols-sm-2 row-cols-lg-3 g-5">
                     <Pollcard title="Lorem ipsum dolor sit amet, consectetur adipiscing elit." creator="Lorem ipsum dolor"/>
                     <Pollcard title="Lorem ipsum dolor sit amet," creator="Lorem ipsum"/>
                     <Pollcard title="Lorem ipsum" creator="Lorem"/>
